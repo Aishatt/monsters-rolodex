a33a14d24c871467afcdadb61423db22eafc9965
aisha <aishatadebayor0@gmail.com>
2022-06-24 09:44:08 -0700

    looping through data

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -4,28 +4,49 @@ import './App.css';
 import { render } from '@testing-library/react';
 
 class App extends Component {
-  constructor()={
+  constructor() {
+    super();
 
+    this.state = {
+      monsters: [
+        {
+          name: 'Thanos',
+          id: '23e456ty'
+        },
+        {
+          name: 'Lucifer',
+          id: '23l856ty'
+        },
+        {
+          name: 'Franklith',
+          id: '45e456ty'
+        },
+        {
+          name: 'crix',
+          id: '23e46hly'
+        }
+      ]
+    };
   }
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, My Name is {this.state.name}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          {
+            this.state.monsters.map((monster) => {
+              return(
+                 <div key={monster.id}>
+                 <h1 >{monster.name}</h1> 
+              </div>
+              );
+              })
+            //console.log(state.monster1)
+          }
+
+        </header>
+      </div>
+    );
+  }
 }
 export default App;
